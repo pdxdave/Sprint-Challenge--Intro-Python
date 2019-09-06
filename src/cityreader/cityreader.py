c579675911508275ae6983f396709bea7b189119
pdxdave <webdevdave@hotmail.com>
2019-09-06 09:53:15 -0700

    starting cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+import cvs
+
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name 
+    self.lat = lat 
+    self.log = lon
+
+  def __repr__(self):
+    return f'{self.name}, {self.lat}, {self.lon}'
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
